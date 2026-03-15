4739dee35919cdc827af2277be9e239c675c77e3
montybytes <muturimonty@gmail.com>
2026-03-15 23:30:48 +0300

    added monospace versions of NType and NDot

--- lib/fonts.dart
+++ lib/fonts.dart
@@ -2,7 +2,9 @@ class NxFonts {
   static const fontInter = "Inter";
   static const fontInterDigits = "InterMonoDigits";
   static const fontNType = "NType";
+  static const fontNTypeDigits = "NTypeMonoDigits";
   static const fontNDot = "NDot";
+  static const fontNDotDigits = "NDotMonoDigits";
   static const fontLettera = "LetteraMono";
   static const fontSpaceMono = "SpaceMono";
   static const fontSpaceGrotesk = "SpaceGrotesk";
